1190aed206ac81708d87228a6f795f389b88c341
shiva123vvv <Shivaneshkumar22@gmail.com>
2025-11-26 21:00:56 +0530

    Add

--- dashboard/execitive_view.py
+++
@@ -1,7 +0,0 @@
-def generate_ceo_dashboard() -> Dict:
-    return {
-        "team_health_index": 78,
-        "burnout_risk_trend": "improving",
-        "productivity_impact": "+23%",
-        "recommended_actions": ["Balance team workload", "Implement focus time"]
-    }
